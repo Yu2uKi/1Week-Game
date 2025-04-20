39b059c1e8b19e82c8c8ac6293344ee377fa6156
Yu2uKi <167176090+Yu2uKi@users.noreply.github.com>
2025-04-20 18:53:10 +0900

    OpeningScene

---
+++ Assets/Scripts/UI/OpeningUIController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+public class OpeningUIController : MonoBehaviour
+{
+    private VisualElement settingsPanel;
+
+    void OnEnable()
+    {
+        var root = GetComponent<UIDocument>().rootVisualElement;
+
+        var startButton = root.Q<Button>("StartButton");
+        var settingsButton = root.Q<Button>("SettingsButton");
+        var closeButton = root.Q<Button>("CloseSettingsButton");
+
+        settingsPanel = root.Q<VisualElement>("SettingsPanel");
+
+        settingsButton.clicked += () => {
+            settingsPanel.style.display = DisplayStyle.Flex;
+        };
+
+        closeButton.clicked += () => {
+            settingsPanel.style.display = DisplayStyle.None;
+        };
+
+        startButton.clicked += () => {
+            Debug.Log("はじめるボタンが押された！");
+            // あとで名前入力シーンに移動
+        };
+    }
+}
