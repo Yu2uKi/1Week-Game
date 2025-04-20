525c91769da136e66511a3a3739dbb0d967464da
Yu2uKi <167176090+Yu2uKi@users.noreply.github.com>
2025-04-20 21:31:42 +0900

    Font＆Type Error

---
+++ Assets/Scripts/UI/NameInputController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
+
+public class NameInputController : MonoBehaviour
+{
+    void OnEnable()
+    {
+        var root = GetComponent<UIDocument>().rootVisualElement;
+
+        var playerInput = root.Q<TextField>("PlayerNameInput");
+        var aiInput = root.Q<TextField>("AINameInput");
+        var confirmButton = root.Q<Button>("ConfirmButton");
+
+        confirmButton.clicked += () => {
+            string playerName = playerInput.text;
+            string aiName = aiInput.text;
+
+            PlayerPrefs.SetString("PlayerName", playerName);
+            PlayerPrefs.SetString("AIName", aiName);
+            PlayerPrefs.Save();
+
+            Debug.Log($"名前保存：{playerName}, {aiName}");
+
+            SceneManager.LoadScene("Personality"); // 次のシーン名（仮）
+        };
+    }
+}
